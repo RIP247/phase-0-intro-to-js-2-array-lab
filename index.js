9626b768281a26d1c60eefa772d602e432c11c8e
Rodney <rodneypa@LAPTOP-KQJ5G51E.localdomain>
2022-02-01 13:31:13 -0500

    commit tree 8aa71bc2ad08a09a9e69d70bfe398027a7a9a445

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(){
+    destructivelyAppendCat = [...cats];
+    cats.push('Ralph');
+};
+
+function destructivelyPrependCat (){
+    destructivelyPrependCat = [...cats];
+    cats.unshift('Bob');
+};
+
+function destructivelyRemoveLastCat(){
+    destructivelyRemoveLastCat = [...cats];
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat(){
+    destructivelyRemoveFirstCat = [...cats];
+    cats.shift();
+};
+
+function appendCat(new_name) {
+    console.log(new_name);
+
+   const appendCat_1 = [...cats];
+   appendCat_1.push(new_name)
+   console.log(appendCat_1);
+
+     return appendCat_1;
+    
+};
+
+function prependCat(name){
+
+    const prependCat_1 = [...cats];
+    prependCat_1.unshift('Arnold')
+    console.log(prependCat_1);
+    return prependCat_1;
+}
+
+function removeLastCat(name) {
+ const removeLastCat_1 = [...cats];
+ removeLastCat_1.pop()
+ return removeLastCat_1;
+}
+
+function removeFirstCat(name){
+    const removeFirstCat_1 = [...cats];
+    removeFirstCat_1.shift();
+    return removeFirstCat_1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
